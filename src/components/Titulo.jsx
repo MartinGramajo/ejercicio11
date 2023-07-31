c80ab2247bf53dff23d013981562ac07b43930e4
¨mago¨ <martingramajo08@gmail.com>
2023-07-31 15:16:19 -0300

    agrego el buscador en component titulo

--- src/components/Titulo.jsx
+++ src/components/Titulo.jsx
@@ -1,6 +1,14 @@
-import { ButtonGroup, Button } from "react-bootstrap";
+import { ButtonGroup, Button, Form } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-const Titulo = ({ setLanguage }) => {
+const Titulo = ({ setLanguage, search, setSearch, handleChangeInput }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearch(search);
+    setSearch("");
+  };
+
   return (
     <section>
       <div className="d-flex justify-content-end me-1 py-1">
@@ -15,6 +23,22 @@ const Titulo = ({ setLanguage }) => {
       </div>
       <div>
         <h1 className="text-center  py-5 fs-1 text-white"> NotiWorld</h1>
+
+        <div className="d-flex justify-content-center">
+          <Form className="w-50" onSubmit={handleSubmit}>
+            <Form.Group className="mb-3 d-flex" controlId="tarea">
+              <Form.Control
+                type="text"
+                placeholder="ingrese una Búsqueda"
+                value={search}
+                onChange={handleChangeInput}
+              />
+              <Button variant="success" type="submit">
+                <FontAwesomeIcon icon={faSearch} />
+              </Button>
+            </Form.Group>
+          </Form>
+        </div>
         <hr className="text-light py-2" />
       </div>
     </section>
